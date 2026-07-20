00645e081ee12a87484b8549eabfc43a70695634
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-07-20 14:19:12 +0100

    creation of category enums

---
+++ vouched-api/src/main/java/com/nicolecohen/vouched/enums/Category.java
@@ -0,0 +1,14 @@
+package com.nicolecohen.vouched.enums;
+
+public enum Category {
+    CAFE,
+    RESTAURANT,
+    BAR,
+    PUB,
+    SALON,
+    SHOP,
+    GYM,
+    GALLERY,
+    PARK,
+    OTHER
+}
