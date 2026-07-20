7d6fe8ed7bc686d3a1ee12e06e2cb647431a5f9b
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-07-20 13:54:14 +0100

    REST controller added for spots

--- vouched-api/src/main/java/com/nicolecohen/vouched/controller/SpotController.java
+++ vouched-api/src/main/java/com/nicolecohen/vouched/controller/SpotController.java
@@ -1,4 +1,34 @@
 package com.nicolecohen.vouched.controller;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/spots")
 public class SpotController {
+
+    @GetMapping
+    public List<String> getAllSpots(){
+        return List.of("Cottiers", "Old School House", "Oran Mor");
+    }
+
+    @GetMapping("/{name}")
+    public String getSpot(@PathVariable String name){
+        return "Spot: " + name;
+    }
+
+    @PostMapping
+    public String createSpot(@RequestBody String spotName){
+        return "Created Spot: " + spotName;
+    }
+
+    @PutMapping("/{name}")
+    public String updateSpot(@PathVariable String name, @RequestBody String newName){
+        return "Updated " + name + " to " + newName;
+    }
+
+    @DeleteMapping("/{name}")
+    public String deleteSpot(@PathVariable String name){
+        return "Deleted spot: " + name;
+    }
 }
