1e3bf0080256a9095b1ad93f4dce747300381189
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-07-21 17:52:09 +0100

    updating SpotService to use the new repository instead of Map<>

--- vouched-api/src/main/java/com/nicolecohen/vouched/service/SpotService.java
+++ vouched-api/src/main/java/com/nicolecohen/vouched/service/SpotService.java
@@ -4,6 +4,8 @@ import com.nicolecohen.vouched.enums.Category;
 import com.nicolecohen.vouched.enums.PrivacyLevel;
 import com.nicolecohen.vouched.exception.*;
 import com.nicolecohen.vouched.model.Spot;
+import com.nicolecohen.vouched.repository.SpotRepository;
+
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,77 +14,32 @@ import java.util.*;
 @Service
 public class SpotService {
 
-    //Map key(UUID) value(Spot) pair - instantaneous spot
-    private final Map<UUID, Spot> spots = new HashMap<>();
+    private final SpotRepository spotRepository;
 
-    //constructor
-    public SpotService() {
-        //hard coding some spots - for testing purposes
-        Spot spot1 = Spot.builder()
-                .id(UUID.randomUUID())
-                .ownerId("user-001")
-                .name("Amulet")
-                .address("5 Partick, Glasgow")
-                .city("Glasgow")
-                .notes("Amazing coffee, cool skater vibes. Better for takeaway, limited seating.")
-                .category(Category.CAFE)
-                .privacyLevel(PrivacyLevel.FRIENDS)
-                .vibeTags(List.of("specialty coffee"))
-                .isVisited(false)
-                .wantsToVisit(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        Spot spot2 = Spot.builder()
-                .id(UUID.randomUUID())
-                .ownerId("user-002")
-                .name("Cottiers")
-                .address("31 Hyndland Road, Glasgow")
-                .city("Glasgow")
-                .notes("Pub in old Kirk, cosy vibes, good outdoor seating.")
-                .category(Category.PUB)
-                .privacyLevel(PrivacyLevel.FRIENDS)
-                .vibeTags(List.of("beer garden", "cool atmosphere", "big groups"))
-                .isVisited(true)
-                .wantsToVisit(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        //adds new entry to the Map
-        spots.put(spot1.getId(), spot1);
-        spots.put(spot2.getId(), spot2);
+    public SpotService(SpotRepository spotRepository){
+        this.spotRepository = spotRepository;
     }
 
     public List<Spot> getAllSpots(){
-        return new ArrayList<>(spots.values());
+        return spotRepository.findAll();
     }
 
     public Spot getSpot(UUID id){
-        Spot spot = spots.get(id);
-        if(spot == null){
-            throw new NotFoundException("Spot not found with this id: " + id);
-        }
-        return spot;
+        return spotRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(
+                        "Spot not found with id: " + id
+                ));
     }
 
+    //CREATE SPOT
     public  Spot createSpot(Spot spot){
-        if (spots.containsKey(spot.getId())){
-            throw new AlreadyExistsException("A spot with this id already exists.");
-        }
-        spots.put(spot.getId(), spot);
-        return spot;
+        return spotRepository.save(spot);
     }
 
     //UPDATE SPOT
     public Spot updateSpot(UUID id, Spot updatedSpot){
 
-        if(!spots.containsKey(updatedSpot.getId())){
-            throw new NotFoundException("Spot not found with this id: " + id);
-        }
-
-        Spot existingSpot = spots.get(id);
+        Spot existingSpot = getSpot(id);
         //fields that are allowed to be updated
         existingSpot.setNotes(updatedSpot.getNotes());
         existingSpot.setPrivacyLevel(updatedSpot.getPrivacyLevel());
@@ -92,16 +49,15 @@ public class SpotService {
         existingSpot.setVisitedDate(updatedSpot.getVisitedDate());
         existingSpot.setUpdatedAt(LocalDateTime.now());
 
-        spots.put(id, existingSpot);
-        return existingSpot;
+        return spotRepository.save(existingSpot);
     }
 
     //DELETE SPOT
     public void deleteSpot(UUID id){
-        if(!spots.containsKey(id)){
+        if(!spotRepository.existsById(id)){
             throw new NotFoundException("Cannot find spot with this id: "+ id);
         }
-        spots.remove(id);
+        spotRepository.deleteById(id);
     }
 
 }
