44a6339ae989e86fd8354b4309a731b08229641d
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-07-22 18:02:12 +0100

    JwtUtil added to create stateless authentication

---
+++ vouched-api/src/main/java/com/nicolecohen/vouched/security/JwtUtil.java
@@ -0,0 +1,52 @@
+package com.nicolecohen.vouched.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    //would normally never be hardcoded - ok for scope of dissertation
+    private static final String SECRET =
+            "vouchedSuperSecretKeyForJwtSigningMustBe256BitsLong!!";
+
+    private static final long EXPIRY_MS = 86400000; // 24 hours until token expires
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    //JWT builder
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRY_MS))
+                .signWith(key)
+                .compact();
+    }
+
+    //JWT reversed -- locate email
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .verifyWith((javax.crypto.SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    //simple wrapper to ensure validity of token
+    public boolean isTokenValid(String token){
+        try{
+            extractEmail(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+    }
+
+}
